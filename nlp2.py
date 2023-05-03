c68ae772a2b2023e4b38b986d6f35b1300e26172
nivedmpayam <125865742+nivedmpayam@users.noreply.github.com>
2023-05-03 18:20:50 +0530

    nlp2.py

--- nlp2.py
+++ nlp2.py
@@ -1,40 +1,43 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.naive_bayes import MultinomialNB
-import streamlit as st
-import pickle
+import streamlit as slt
 
-# Read the dataset
-bbc_text = pd.read_csv(r"D:\STUDY\Sem2\NLP\NLPIR\bbc-text.txt")
-bbc_text = bbc_text.rename(columns={'text': 'News_Headline'}, inplace=False)
+bbc_text = pd.read_csv('bbc-text.txt')
+bbc_text=bbc_text.rename(columns = {'text': 'News_Headline'}, inplace = False)
+bbc_text.head()
 bbc_text.category = bbc_text.category.map({'tech':0, 'business':1, 'sport':2, 'entertainment':3, 'politics':4})
+bbc_text.category.unique()
 
-# Split the data
+from sklearn.model_selection import train_test_split
 X = bbc_text.News_Headline
 y = bbc_text.category
-X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.6, random_state=1)
+#split data
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size = 0.6, random_state = 1)
 
-# Vectorize the text
-vector = CountVectorizer(stop_words='english', lowercase=False)
+from sklearn.feature_extraction.text import CountVectorizer
+vector = CountVectorizer(stop_words = 'english',lowercase=False)
+# fit the vectorizer on the training data
 vector.fit(X_train)
 X_transformed = vector.transform(X_train)
+X_transformed.toarray()
+# for test data
 X_test_transformed = vector.transform(X_test)
 
-# Train the model
+from sklearn.naive_bayes import MultinomialNB
 naivebayes = MultinomialNB()
 naivebayes.fit(X_transformed, y_train)
 
-# Save the model
-with open('model.pkl', 'wb') as f:
-    pickle.dump(naivebayes, f)
+import pickle
 
-# Define the Streamlit app
-st.header('Demo')
-input_text = st.text_area("Please enter the text", value="")
-if st.button("Check"):
-    input_text_transformed = vector.transform([input_text]).toarray()
-    prediction = naivebayes.predict(input_text_transformed)[0]
-    prediction_mapping = {0: 'TECH', 1: 'BUSINESS', 2: 'SPORTS', 3: 'ENTERTAINMENT', 4: 'POLITICS'}
-    result = prediction_mapping[prediction]
-    st.write(f"Predicted category: {result}")
+saved_model = pickle.dumps(naivebayes)
+s = pickle.loads(saved_model)
+
+
+slt.header("News Categorizer")
+input=slt.text_input("Enter the text")
+
+inp=vector.transform([input]).toarray()
+    
+
+if slt.button("Predict"):
+    pre=(str(list(s.predict(inp))[0]).replace('0', 'TECH').replace('1', 'BUSINESS').replace('2', 'SPORTS').replace('3','ENTERTAINMENT').replace('4','POLITICS'))
+    slt.write(pre)
